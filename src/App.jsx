83a2579fbbf6ffd1c2707584b0a51c123818062d
Shubham Das <subhamdas9800@gmail.com>
2024-12-20 16:29:21 +0530

    logo updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Signin from './Pages/Signin'
+
 import Signup from './Pages/Signup'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Homepage from './Pages/Homepage'
